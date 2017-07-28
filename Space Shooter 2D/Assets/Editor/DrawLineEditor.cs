51971f527f52caa64ba2eb26f916fe157b2b8e11
Roman Vylegzhanin <godlikemo@mail.ru>
2017-07-28 19:58:54 +0300

    Added last packages to repo

---
+++ Space Shooter 2D/Assets/Editor/DrawLineEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+[CustomEditor(typeof(AI))]
+public class DrawLineEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        AI t = target as AI;
+
+        if (t == null || t.waypointTr == null)
+            return;
+
+        Vector3 center = t.transform.position;
+        Handles.color = Color.green;
+        for (int i = 0; i < t.waypointTr.Length; i++)
+        {
+            if (t.waypointTr[i] != null)
+                Handles.DrawLine(center, t.waypointTr[i].transform.position);
+        }
+        Handles.color = Color.clear;
+    }
+}
---
+++ Space Shooter 2D/Assets/Editor/FOVEditorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(FOVScript))]
+public class FOVEditorScript : Editor
+{
+
+    void OnSceneGUI()
+    {
+        FOVScript fov = (FOVScript)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.back, Vector3.right, 360, fov.viewRadius);
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fov.visibleTargets)
+        {
+            if (visibleTarget != null)
+                Handles.DrawLine(fov.transform.position, visibleTarget.position);
+        }
+    }
+
+}
